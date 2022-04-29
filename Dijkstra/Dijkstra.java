b1be4e6a9654ad59edf2f51e0da2532c95256aba
Lars Husfloen <lhusfloen@live.no>
2022-04-29 23:50:22 +0200

    Implementert lambda. Utfaktorert en metode for å minke kompleksitet

--- Dijkstra/Dijkstra.java
+++ Dijkstra/Dijkstra.java
@@ -1,16 +1,17 @@
 package Dijkstra;
+
 import java.util.*;
 
 public class Dijkstra {
-    public static Dictionary[] dijkstra (Graph g, Vertex startingVertex){
+    public static Dictionary[] dijkstra(Graph g, Vertex startingVertex) {
         Dictionary<String, Integer> distances = new Hashtable<>();
         Dictionary<String, Vertex> previous = new Hashtable<>();
         PriorityQueue<QueueObject> queue = new PriorityQueue<>();
 
         queue.add(new QueueObject(startingVertex, 0));
 
-        for (Vertex v: g.getVertices()) {
-            if(v != startingVertex){
+        for (Vertex v : g.getVertices()) {
+            if (v != startingVertex) {
                 distances.put(v.getData(), Integer.MAX_VALUE);
             }
             previous.put(v.getData(), new Vertex("Null"));
@@ -18,22 +19,27 @@ public class Dijkstra {
 
         distances.put(startingVertex.getData(), 0);
 
-        while(queue.size() != 0){
-            Vertex current = queue.poll().vertex;
-            for (Edge e: current.getEdges()) {
-                Integer alternate = distances.get(current.getData()) + e.getWeight();
-                String neighborValue = e.getEnd().getData();
-                if (alternate < distances.get(neighborValue)){
-                    distances.put(neighborValue, alternate);
-                    previous.put(neighborValue, current);
-                    queue.add(new QueueObject(e.getEnd(), distances.get(neighborValue)));
-                }
-            }
-        }
+        run(distances, previous, queue);
+
         return new Dictionary[]{distances, previous};
     }
 
-    public static void shortestPathBetween(Graph g, Vertex startingVertex, Vertex targetVertex){
+    private static void run(Dictionary<String, Integer> distances, Dictionary<String, Vertex> previous, PriorityQueue<QueueObject> queue) {
+        while (!queue.isEmpty()) {
+            Vertex current = queue.poll().vertex;
+            current.getEdges().forEach(edge -> {
+                Integer alternate = distances.get(current.getData()) + edge.getWeight();
+                String neighborValue = edge.getEnd().getData();
+                if (alternate < distances.get(neighborValue)) {
+                    distances.put(neighborValue, alternate);
+                    previous.put(neighborValue, current);
+                    queue.add(new QueueObject(edge.getEnd(), distances.get(neighborValue)));
+                }
+            });
+        }
+    }
+
+    public static void shortestPathBetween(Graph g, Vertex startingVertex, Vertex targetVertex) {
         Dictionary[] dijkstraDictionaries = dijkstra(g, startingVertex);
         var distances = dijkstraDictionaries[0];
         var previous = dijkstraDictionaries[1];
@@ -43,28 +49,29 @@ public class Dijkstra {
         System.out.println(shortestPath(targetVertex, previous) + "\tDistance: " + distance);
     }
 
-    public static void dijkstraResultPrinter(Dictionary[] d){
+    public static void dijkstraResultPrinter(Dictionary[] d) {
         /* Viser den korteste distansen mellom gjeldene vertex og alle andre */
         System.out.println("Distances to:");
-        for (var keys = d[0].keys(); keys.hasMoreElements();){
+        for (var keys = d[0].keys(); keys.hasMoreElements(); ) {
             String nextKey = keys.nextElement().toString();
             System.out.println(nextKey + ": " + d[0].get(nextKey));
         }
         System.out.println(" ");
     }
 
-    private static String shortestPath(Vertex targetVertex, Dictionary previous){
+    private static String shortestPath(Vertex targetVertex, Dictionary<Graph, Vertex> previous) {
         ArrayList<Vertex> path = new ArrayList<>();
         Vertex v = targetVertex;
-        while(!Objects.equals(v.getData(), "Null")){
-            path.add(0,v);
-            v = (Vertex) previous.get(v.getData());
+        while (!Objects.equals(v.getData(), "Null")) {
+            path.add(0, v);
+            v = previous.get(v.getData());
         }
         StringBuilder shortestPath = new StringBuilder();
-        for (Vertex pathVertex: path){
-            shortestPath.append(pathVertex.getData());
+        path.forEach(vertex -> {
+            shortestPath.append(vertex.getData());
             shortestPath.append("-");
-        }
+        });
+
         shortestPath.setLength(shortestPath.length() - 1);
         return "Path: " + "[" + shortestPath + "] ";
     }
